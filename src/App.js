2bac088651eb754d12a578834859e0f226917c73
Carmen Villalobos <carmenvillalobos@Carmens-MBP.attlocal.net>
2022-12-04 12:26:07 -0800

    Completed project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react'
+import Gallery from './Components/Gallery';
+import ButtonBar from './Components/ButtonBar';
 
 function App() {
+  let [artId, setArtId] = useState(5980)
+  let [data, setData] = useState({})
+
+useEffect(() => {
+    document.title='Welcome to Artworld'
+    fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${artId}`)
+    .then(response => response.json())
+    .then(resData => setData(resData))
+    }, [artId])
+
+    const handleIterate = e => {
+      setArtId(artId + Number(e.target.value))
+    }
+
   return (
     <div className="App">
-      
+      <Gallery data = {data}/>
+      <ButtonBar handleIterate={handleIterate} />    
     </div>
   );
 }
